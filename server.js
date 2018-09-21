2c4ab4793acca368feb65bc82a768e37c0902076
zhaoyun <zhaoyun@ttk.com>
2018-09-21 17:41:05 +0800

    集成expresss

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const proxy = require('http-proxy-middleware');
+
+const app = express()
+
+
+
+app.use('/v1', proxy({ target: 'http://127.0.0.1:8080/', changeOrigin: true }))
+
+app.use(express.static('dist'))
+
+app.listen(8080)
