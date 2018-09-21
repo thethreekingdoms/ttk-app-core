d514578bda98773d184af0af921fe8265ba03179
zhaoyun <zhaoyun@ttk.com>
2018-09-21 16:25:35 +0800

    test

--- compatible/server.js
+++ compatible/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+// const express = require('express');
 
-const proxy = require('http-proxy-middleware');
+// const proxy = require('http-proxy-middleware');
 
-const app = express()
+// const app = express()
 
 
 
-app.use('/v1', proxy({target: 'http://127.0.0.1:8080/', changeOrigin: true}))
+// app.use('/v1', proxy({target: 'http://127.0.0.1:8080/', changeOrigin: true}))
 
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 
-app.listen(8080)
+// app.listen(8080)
