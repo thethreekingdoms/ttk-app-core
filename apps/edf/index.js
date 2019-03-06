6a99b7e21a476813c4f169768b3e13e6bec85197
caixiaobing <caixiaobing@foresee.com.cn>
2019-03-06 14:05:28 +0800

    ttk rewrite

--- apps/edf/index.js
+++ apps/edf/index.js
@@ -1,103 +1,104 @@
-import ttk_edf_app_card_account from './ttk-edf-app-card-account'
-import ttk_edf_app_card_currency from './ttk-edf-app-card-currency'
-import ttk_edf_app_card_customer from './ttk-edf-app-card-customer'
-import ttk_edf_app_card_department from './ttk-edf-app-card-department'
-import ttk_edf_app_card_inventory from './ttk-edf-app-card-inventory'
-import ttk_edf_app_card_project from './ttk-edf-app-card-project'
-import ttk_edf_app_card_supplier from './ttk-edf-app-card-supplier'
-import ttk_edf_app_card_userdefinecard from './ttk-edf-app-card-userdefinecard'
-import ttk_edf_app_column from './ttk-edf-app-column'
-import edfx_app_column_detail from './ttk-edf-app-column/apps/edfx-app-column-detail'
-import edfx_app_column_setting from './ttk-edf-app-column/apps/edfx-app-column-setting'
-import edfx_app_column_type from './ttk-edf-app-column/apps/edfx-app-column-type'
-import ttk_edf_app_company_manage from './ttk-edf-app-company-manage'
-import ttk_edf_app_company_manage_add from './ttk-edf-app-company-manage/apps/ttk-edf-app-company-manage-add'
-// import ttk_edf_app_devtools from './ttk-edf-app-devtools'
-import ttk_edf_app_enum from './ttk-edf-app-enum'
-import edfx_app_tree_table_detail from './ttk-edf-app-enum/apps/edfx-app-tree-table-detail'
-import edfx_app_tree_table_type from './ttk-edf-app-enum/apps/edfx-app-tree-table-type'
-import ttk_edf_app_forgot_password from './ttk-edf-app-forgot-password'
-import ttk_edf_app_home from './ttk-edf-app-home'
-import ttk_edf_app_hot_search_widget from './ttk-edf-app-home/apps/ttk-edf-app-hot-search-widget'
-import ttk_edf_app_market_widget from './ttk-edf-app-home/apps/ttk-edf-app-market-widget'
-import ttk_edf_app_sale_proportion_widget from './ttk-edf-app-home/apps/ttk-edf-app-sale-proportion-widget'
-import ttk_edf_app_sale_trend_widget from './ttk-edf-app-home/apps/ttk-edf-app-sale-trend-widget'
-import ttk_edf_app_sale_widget from './ttk-edf-app-home/apps/ttk-edf-app-sale-widget'
-import ttk_edf_app_trade_widget from './ttk-edf-app-home/apps/ttk-edf-app-trade-widget'
-import ttk_edf_app_visit_widget from './ttk-edf-app-home/apps/ttk-edf-app-visit-widget'
-import ttk_edf_app_hotloader from './ttk-edf-app-hotloader'
-import ttk_edf_app_iframe from './ttk-edf-app-iframe'
-import ttk_edf_app_im from './ttk-edf-app-im'
-import ttk_edf_app_login from './ttk-edf-app-login'
-import ttk_edf_app_my_setting from './ttk-edf-app-my-setting'
-import ttk_edf_app_my_setting_change_mobile from './ttk-edf-app-my-setting/apps/ttk-edf-app-my-setting-change-mobile'
-import ttk_edf_app_my_setting_change_password from './ttk-edf-app-my-setting/apps/ttk-edf-app-my-setting-change-password'
-import ttk_edf_app_notice from './ttk-edf-app-notice'
-import ttk_edf_app_operation from './ttk-edf-app-operation'
-import ttk_edf_app_pagestyle from './ttk-edf-app-pagestyle'
-import ttk_edf_app_portal from './ttk-edf-app-portal'
-import ttk_edf_app_portal_menu from './ttk-edf-app-portal-menu'
-import ttk_edf_app_portal_menu_detail from './ttk-edf-app-portal-menu-detail'
-import ttk_edf_app_register from './ttk-edf-app-register'
-import ttk_edf_app_agreement from './ttk-edf-app-register/apps/ttk-edf-app-agreement'
-import ttk_edf_app_role from './ttk-edf-app-role'
-import ttk_edf_app_role_auth from './ttk-edf-app-role-auth'
-import ttk_edf_app_root from './ttk-edf-app-root'
-import ttk_edf_app_voucher from './ttk-edf-app-voucher'
-import ttk_edf_app_voucher_detail_setting from './ttk-edf-app-voucher/apps/ttk-edf-app-voucher-detail-setting'
-import ttk_edf_app_voucher_setting from './ttk-edf-app-voucher/apps/ttk-edf-app-voucher-setting'
+//此文件由ttk rewrite命令工具生成
+import ttk_edf_app_card_account from  './ttk-edf-app-card-account'
+import ttk_edf_app_card_currency from  './ttk-edf-app-card-currency'
+import ttk_edf_app_card_customer from  './ttk-edf-app-card-customer'
+import ttk_edf_app_card_department from  './ttk-edf-app-card-department'
+import ttk_edf_app_card_inventory from  './ttk-edf-app-card-inventory'
+import ttk_edf_app_card_project from  './ttk-edf-app-card-project'
+import ttk_edf_app_card_supplier from  './ttk-edf-app-card-supplier'
+import ttk_edf_app_card_userdefinecard from  './ttk-edf-app-card-userdefinecard'
+import ttk_edf_app_column from  './ttk-edf-app-column'
+import edfx_app_column_detail from  './ttk-edf-app-column/apps/edfx-app-column-detail'
+import edfx_app_column_setting from  './ttk-edf-app-column/apps/edfx-app-column-setting'
+import edfx_app_column_type from  './ttk-edf-app-column/apps/edfx-app-column-type'
+import ttk_edf_app_company_manage from  './ttk-edf-app-company-manage'
+import ttk_edf_app_company_manage_add from  './ttk-edf-app-company-manage/apps/ttk-edf-app-company-manage-add'
+import ttk_edf_app_devtools from  './ttk-edf-app-devtools'
+import ttk_edf_app_enum from  './ttk-edf-app-enum'
+import edfx_app_tree_table_detail from  './ttk-edf-app-enum/apps/edfx-app-tree-table-detail'
+import edfx_app_tree_table_type from  './ttk-edf-app-enum/apps/edfx-app-tree-table-type'
+import ttk_edf_app_forgot_password from  './ttk-edf-app-forgot-password'
+import ttk_edf_app_home from  './ttk-edf-app-home'
+import ttk_edf_app_hot_search_widget from  './ttk-edf-app-home/apps/ttk-edf-app-hot-search-widget'
+import ttk_edf_app_market_widget from  './ttk-edf-app-home/apps/ttk-edf-app-market-widget'
+import ttk_edf_app_sale_proportion_widget from  './ttk-edf-app-home/apps/ttk-edf-app-sale-proportion-widget'
+import ttk_edf_app_sale_trend_widget from  './ttk-edf-app-home/apps/ttk-edf-app-sale-trend-widget'
+import ttk_edf_app_sale_widget from  './ttk-edf-app-home/apps/ttk-edf-app-sale-widget'
+import ttk_edf_app_trade_widget from  './ttk-edf-app-home/apps/ttk-edf-app-trade-widget'
+import ttk_edf_app_visit_widget from  './ttk-edf-app-home/apps/ttk-edf-app-visit-widget'
+import ttk_edf_app_hotloader from  './ttk-edf-app-hotloader'
+import ttk_edf_app_iframe from  './ttk-edf-app-iframe'
+import ttk_edf_app_im from  './ttk-edf-app-im'
+import ttk_edf_app_login from  './ttk-edf-app-login'
+import ttk_edf_app_my_setting from  './ttk-edf-app-my-setting'
+import ttk_edf_app_my_setting_change_mobile from  './ttk-edf-app-my-setting/apps/ttk-edf-app-my-setting-change-mobile'
+import ttk_edf_app_my_setting_change_password from  './ttk-edf-app-my-setting/apps/ttk-edf-app-my-setting-change-password'
+import ttk_edf_app_notice from  './ttk-edf-app-notice'
+import ttk_edf_app_operation from  './ttk-edf-app-operation'
+import ttk_edf_app_pagestyle from  './ttk-edf-app-pagestyle'
+import ttk_edf_app_portal from  './ttk-edf-app-portal'
+import ttk_edf_app_portal_menu from  './ttk-edf-app-portal-menu'
+import ttk_edf_app_portal_menu_detail from  './ttk-edf-app-portal-menu-detail'
+import ttk_edf_app_register from  './ttk-edf-app-register'
+import ttk_edf_app_agreement from  './ttk-edf-app-register/apps/ttk-edf-app-agreement'
+import ttk_edf_app_role from  './ttk-edf-app-role'
+import ttk_edf_app_role_auth from  './ttk-edf-app-role-auth'
+import ttk_edf_app_root from  './ttk-edf-app-root'
+import ttk_edf_app_voucher from  './ttk-edf-app-voucher'
+import ttk_edf_app_voucher_detail_setting from  './ttk-edf-app-voucher/apps/ttk-edf-app-voucher-detail-setting'
+import ttk_edf_app_voucher_setting from  './ttk-edf-app-voucher/apps/ttk-edf-app-voucher-setting'
 
-const obj = {
-  [ttk_edf_app_card_account.name]: ttk_edf_app_card_account,
-  [ttk_edf_app_card_currency.name]: ttk_edf_app_card_currency,
-  [ttk_edf_app_card_customer.name]: ttk_edf_app_card_customer,
-  [ttk_edf_app_card_department.name]: ttk_edf_app_card_department,
-  [ttk_edf_app_card_inventory.name]: ttk_edf_app_card_inventory,
-  [ttk_edf_app_card_project.name]: ttk_edf_app_card_project,
-  [ttk_edf_app_card_supplier.name]: ttk_edf_app_card_supplier,
-  [ttk_edf_app_card_userdefinecard.name]: ttk_edf_app_card_userdefinecard,
-  [ttk_edf_app_column.name]: ttk_edf_app_column,
-  [edfx_app_column_detail.name]: edfx_app_column_detail,
-  [edfx_app_column_setting.name]: edfx_app_column_setting,
-  [edfx_app_column_type.name]: edfx_app_column_type,
-  [ttk_edf_app_company_manage.name]: ttk_edf_app_company_manage,
-  [ttk_edf_app_company_manage_add.name]: ttk_edf_app_company_manage_add,
-  //[ttk_edf_app_devtools.name]: ttk_edf_app_devtools,
-  [ttk_edf_app_enum.name]: ttk_edf_app_enum,
-  [edfx_app_tree_table_detail.name]: edfx_app_tree_table_detail,
-  [edfx_app_tree_table_type.name]: edfx_app_tree_table_type,
-  [ttk_edf_app_forgot_password.name]: ttk_edf_app_forgot_password,
-  [ttk_edf_app_home.name]: ttk_edf_app_home,
-  [ttk_edf_app_hot_search_widget.name]: ttk_edf_app_hot_search_widget,
-  [ttk_edf_app_market_widget.name]: ttk_edf_app_market_widget,
-  [ttk_edf_app_sale_proportion_widget.name]: ttk_edf_app_sale_proportion_widget,
-  [ttk_edf_app_sale_trend_widget.name]: ttk_edf_app_sale_trend_widget,
-  [ttk_edf_app_sale_widget.name]: ttk_edf_app_sale_widget,
-  [ttk_edf_app_trade_widget.name]: ttk_edf_app_trade_widget,
-  [ttk_edf_app_visit_widget.name]: ttk_edf_app_visit_widget,
-  [ttk_edf_app_hotloader.name]: ttk_edf_app_hotloader,
-  [ttk_edf_app_iframe.name]: ttk_edf_app_iframe,
-  [ttk_edf_app_im.name]: ttk_edf_app_im,
-  [ttk_edf_app_login.name]: ttk_edf_app_login,
-  [ttk_edf_app_my_setting.name]: ttk_edf_app_my_setting,
-  [ttk_edf_app_my_setting_change_mobile.name]: ttk_edf_app_my_setting_change_mobile,
-  [ttk_edf_app_my_setting_change_password.name]: ttk_edf_app_my_setting_change_password,
-  [ttk_edf_app_notice.name]: ttk_edf_app_notice,
-  [ttk_edf_app_operation.name]: ttk_edf_app_operation,
-  [ttk_edf_app_pagestyle.name]: ttk_edf_app_pagestyle,
-  [ttk_edf_app_portal.name]: ttk_edf_app_portal,
-  [ttk_edf_app_portal_menu.name]: ttk_edf_app_portal_menu,
-  [ttk_edf_app_portal_menu_detail.name]: ttk_edf_app_portal_menu_detail,
-  [ttk_edf_app_register.name]: ttk_edf_app_register,
-  [ttk_edf_app_agreement.name]: ttk_edf_app_agreement,
-  [ttk_edf_app_role.name]: ttk_edf_app_role,
-  [ttk_edf_app_role_auth.name]: ttk_edf_app_role_auth,
-  [ttk_edf_app_root.name]: ttk_edf_app_root,
-  [ttk_edf_app_voucher.name]: ttk_edf_app_voucher,
-  [ttk_edf_app_voucher_detail_setting.name]: ttk_edf_app_voucher_detail_setting,
-  [ttk_edf_app_voucher_setting.name]: ttk_edf_app_voucher_setting,
+const obj={
+    [ttk_edf_app_card_account.name]: ttk_edf_app_card_account,
+    [ttk_edf_app_card_currency.name]: ttk_edf_app_card_currency,
+    [ttk_edf_app_card_customer.name]: ttk_edf_app_card_customer,
+    [ttk_edf_app_card_department.name]: ttk_edf_app_card_department,
+    [ttk_edf_app_card_inventory.name]: ttk_edf_app_card_inventory,
+    [ttk_edf_app_card_project.name]: ttk_edf_app_card_project,
+    [ttk_edf_app_card_supplier.name]: ttk_edf_app_card_supplier,
+    [ttk_edf_app_card_userdefinecard.name]: ttk_edf_app_card_userdefinecard,
+    [ttk_edf_app_column.name]: ttk_edf_app_column,
+    [edfx_app_column_detail.name]: edfx_app_column_detail,
+    [edfx_app_column_setting.name]: edfx_app_column_setting,
+    [edfx_app_column_type.name]: edfx_app_column_type,
+    [ttk_edf_app_company_manage.name]: ttk_edf_app_company_manage,
+    [ttk_edf_app_company_manage_add.name]: ttk_edf_app_company_manage_add,
+    [ttk_edf_app_devtools.name]: ttk_edf_app_devtools,
+    [ttk_edf_app_enum.name]: ttk_edf_app_enum,
+    [edfx_app_tree_table_detail.name]: edfx_app_tree_table_detail,
+    [edfx_app_tree_table_type.name]: edfx_app_tree_table_type,
+    [ttk_edf_app_forgot_password.name]: ttk_edf_app_forgot_password,
+    [ttk_edf_app_home.name]: ttk_edf_app_home,
+    [ttk_edf_app_hot_search_widget.name]: ttk_edf_app_hot_search_widget,
+    [ttk_edf_app_market_widget.name]: ttk_edf_app_market_widget,
+    [ttk_edf_app_sale_proportion_widget.name]: ttk_edf_app_sale_proportion_widget,
+    [ttk_edf_app_sale_trend_widget.name]: ttk_edf_app_sale_trend_widget,
+    [ttk_edf_app_sale_widget.name]: ttk_edf_app_sale_widget,
+    [ttk_edf_app_trade_widget.name]: ttk_edf_app_trade_widget,
+    [ttk_edf_app_visit_widget.name]: ttk_edf_app_visit_widget,
+    [ttk_edf_app_hotloader.name]: ttk_edf_app_hotloader,
+    [ttk_edf_app_iframe.name]: ttk_edf_app_iframe,
+    [ttk_edf_app_im.name]: ttk_edf_app_im,
+    [ttk_edf_app_login.name]: ttk_edf_app_login,
+    [ttk_edf_app_my_setting.name]: ttk_edf_app_my_setting,
+    [ttk_edf_app_my_setting_change_mobile.name]: ttk_edf_app_my_setting_change_mobile,
+    [ttk_edf_app_my_setting_change_password.name]: ttk_edf_app_my_setting_change_password,
+    [ttk_edf_app_notice.name]: ttk_edf_app_notice,
+    [ttk_edf_app_operation.name]: ttk_edf_app_operation,
+    [ttk_edf_app_pagestyle.name]: ttk_edf_app_pagestyle,
+    [ttk_edf_app_portal.name]: ttk_edf_app_portal,
+    [ttk_edf_app_portal_menu.name]: ttk_edf_app_portal_menu,
+    [ttk_edf_app_portal_menu_detail.name]: ttk_edf_app_portal_menu_detail,
+    [ttk_edf_app_register.name]: ttk_edf_app_register,
+    [ttk_edf_app_agreement.name]: ttk_edf_app_agreement,
+    [ttk_edf_app_role.name]: ttk_edf_app_role,
+    [ttk_edf_app_role_auth.name]: ttk_edf_app_role_auth,
+    [ttk_edf_app_root.name]: ttk_edf_app_root,
+    [ttk_edf_app_voucher.name]: ttk_edf_app_voucher,
+    [ttk_edf_app_voucher_detail_setting.name]: ttk_edf_app_voucher_detail_setting,
+    [ttk_edf_app_voucher_setting.name]: ttk_edf_app_voucher_setting,
+}
 
-};
 window.publicModule && window.publicModule.callback(obj, "edf");
 
 export default obj;
